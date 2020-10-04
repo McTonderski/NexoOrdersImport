300d9f30b075b24b0c61388e28d177282b85b66b
Maciej Tonderski <maciejtonderski.mt@gmai.com>
2020-10-04 14:59:04 +0200

    added anti crash Consequenes system

--- Program.cs
+++ Program.cs
@@ -400,6 +400,20 @@ namespace NexoTestApp
                 Thread.Sleep(900000);
             }
         }
+
+        public static string checklastrun()
+        {
+            /// This code is used to calculate time in minutes between last runned program and now. 
+            /// This is only run on the first use of programm. 
+            DateTime logTime = DateTime.Parse(File.ReadAllText("lastsync.txt"));
+            DateTime now = DateTime.Now;
+            TimeSpan span = now.Subtract(logTime);
+            var temp = span.Minutes + (span.Hours*60) + (span.Days*3600);
+            Console.WriteLine(temp);
+            string a = temp.ToString();
+            return a;
+        }
+
         }
 
     }
