327d815d4e3e3badcc40643a291df3d16b929373
Maciej Tonderski <maciejtonderski.mt@gmai.com>
2020-10-04 14:50:11 +0200

    added Program class and database query strings

--- Program.cs
+++ Program.cs
@@ -103,6 +103,136 @@ namespace NexoTestApp
         }
     }
 
+    class Program
+    {
+        static int last_ETL_runId = 3479461;
+        static string conn_string = @"Server=SERWER; Database=DB; USER=USER; password=PASSWORD; MultipleActiveResultSets=true;";
+        static string query_get_orders = "SELECT [ZamowienieKlientHashKey]," +
+            " [ZamowienieIdHashKey]," +
+            " [KlientIdHashKey]," +
+            " [ETL_RunId]," +
+            " [RecordSource]," +
+            " [ServerId]," +
+            " [Id]," +
+            " [id_klienta]," +
+            " [mail]" +
+            " FROM [Dinar].[raw].[LinkZamowienieKlient]" +
+            " WHERE ETL_RunId > " + last_ETL_runId +
+            " group by ZamowienieIdHashKey, ZamowienieKlientHashKey, KlientIdHashKey, LoadDate, ETL_RunId, RecordSource, ServerId, Id, id_klienta, mail" +
+            " order by ETL_RunId desc";
+
+        static string query_get_pozycja_zamowienie_opakowanie = "SELECT [PozycjaZamowienieOpakowanieHashKey]," +
+            " [PozycjaIdHashKey]," +
+            " [ZamowienieIdHashKey]," +
+            " [OpakowanieIdHashKey]," +
+            " [LoadDate]," +
+            " [ETL_RunId]," +
+            " [RecordSource]," +
+            " [ServerId]," +
+            " [Id]," +
+            " [id_zamowienia]," +
+            " [id_opakowania]" +
+            " FROM [Dinar].[raw].[LinkPozycjaZamowienieOpakowanie]" +
+            " WHERE ETL_RunID > " + last_ETL_runId +
+            " ORDER BY ETL_RunId asc";
+
+        static string query_SatZamowienieDane = "SELECT [ZamowienieIdHashKey]," +
+            " [ETL_RunId]," +
+            " [LoadDate]," +
+            " [LoadEndDate]," +
+            " [RecordSource]," +
+            " [data]," +
+            " [czy_zrealizowano]," +
+            " [platnosc]," +
+            " [uwagi]," +
+            " [potwierdz]," +
+            " [suma]," +
+            " [karta]," +
+            " [koszt_przesylki]," +
+            " [dostawa]," +
+            " [aktywny]," +
+            " [status]," +
+            " [koszt_przesylki_netto]," +
+            " [koszt_przesylki_vat]," +
+            " [ZamowienieDaneHashDiff]" +
+            " FROM [Dinar].[raw].[SatZamowienieDane]" +
+            " WHERE ETL_RunId > " + last_ETL_runId + 
+            " Order By ETL_RunID desc";
+
+        static string query_adresy = "SELECT [ZamowienieIdHashKey]," +
+            " [ETL_RunId]," +
+            " [LoadDate]," +
+            " [LoadEndDate]," +
+            " [RecordSource]," +
+            " [adres]," +
+            " [adres2]," +
+            " [kod]," +
+            " [miasto]," +
+            " [kraj]," +
+            " [imie]," +
+            " [nazwisko]," +
+            " [firma]," +
+            " [ZamowienieKlientAdresHashDiff]" +
+            " FROM [Dinar].[raw].[SatZamowienieKlientAdres]" +
+            " WHERE ETL_RunId > " + last_ETL_runId +
+            " ORDER BY ETL_RunId desc";
+
+        static string query_zamowienie_klient_dane = "SELECT [ZamowienieIdHashKey]," +
+            " [ETL_RunId]," +
+            " [LoadDate]," +
+            " [LoadEndDate]," +
+            " [RecordSource]," +
+            " [imie]," +
+            " [nazwisko]," +
+            " [zam_osoba]," +
+            " [telefon]," +
+            " [fax]," +
+            " [mail]," +
+            " [data]," +
+            " [anonymized]," +
+            " [ZamowienieKlientDaneHashDiff]" +
+            " FROM [Dinar].[raw].[SatZamowienieKlientDane]" +
+            " WHERE ETL_RunId > " + last_ETL_runId +
+            " ORDER BY ETL_RunId desc";
+
+        static string query_zamowienie_klient_dane_firmy = "SELECT [ZamowienieIdHashKey]," +
+            " [ETL_RunId]," +
+            " [LoadDate]," +
+            " [LoadEndDate]," +
+            " [RecordSource]," +
+            " [nip]," +
+            " [firma_nazwa]," +
+            " [firma_imie]," +
+            " [firma_nazwisko]," +
+            " [firma_adres]," +
+            " [firma_adres2]," +
+            " [firma_kod]," +
+            " [firma_miasto]," +
+            " [firma_kraj]," +
+            " [firma_telefon]," +
+            " [firma_email]," +
+            " [ZamowienieKlientDaneFirmyHashDiff]" +
+            " FROM [Dinar].[raw].[SatZamowienieKlientDaneFirmy]" +
+            " WHERE ETL_RunId > " + last_ETL_runId +
+            " ORDER BY ETL_RunId desc";
+
+       static string query_pozycja = "" +
+            "SELECT [PozycjaIdHashKey]," +
+            " [ETL_RunId]," +
+            " [LoadDate]," +
+            " [LoadEndDate]," +
+            " [RecordSource]," +
+            " [liczba]," +
+            " [tytul]," +
+            " [cena]," +
+            " [vat]," +
+            " [cena_netto]," +
+            " [PozycjaHashDiff]," +
+            " [wartosc_vat] " +
+            "FROM [Dinar].[raw].[SatPozycja] " +
+            "WHERE ETL_RunID > " + last_ETL_runId +
+            "ORDER BY ETL_RunId desc";
+
         }
 
     }
